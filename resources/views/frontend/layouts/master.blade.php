1df48eeba953d36f4f0b3d93dfa1965d237be783
nguyenchitai16092004 <nguyenchitai16092004@gmail.com>
2024-11-29 15:34:59 +0700

    Cắt Layout

---
+++ resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Essence - Fashion Ecommerce Template</title>
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <link rel="stylesheet" href="css/core-style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    @include('frontend.partials.header')
+    @yield('main')
+    @include('frontend.partials.footer')
+
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/classy-nav.min.js"></script>
+    <script src="js/active.js"></script>
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('frontend.pages.home');
+});
+Route::get('/shop', function () {
+    return view('frontend.pages.shop');
+});
+Route::get('/single-product-detail', function () {
+    return view('frontend.pages.single-product-details');
+});
+Route::get('/checkout', function () {
+    return view('frontend.pages.checkout');
+});
+Route::get('/blog', function () {
+    return view('frontend.pages.blog');
+});
+Route::get('/single-blog', function () {
+    return view('frontend.pages.single-blog');
+});
+Route::get('/regular-page', function () {
+    return view('frontend.pages.regular-page');
+});
+Route::get('/contact', function () {
+    return view('frontend.pages.contact');
+});
