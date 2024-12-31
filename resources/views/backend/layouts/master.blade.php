b3cead8f1f8e6c502cff8e6451f9ff3c628731b2
ThanhTai15 <0306221264@caothang.edu.vn>
2024-12-31 14:38:47 +0700

    sửa giao diện admin

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -8,6 +8,8 @@
     <title>Dashboard V.3 | Kiaalap - Kiaalap Admin Template</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
     <!-- favicon
   ============================================ -->
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
@@ -63,6 +65,13 @@
     <!-- modernizr JS
   ============================================ -->
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    <!-- footer
+  ============================================ -->
+  <link rel="stylesheet" href="css/footer.css">
+   <!-- header
+  ============================================ -->
+  <link rel="stylesheet" href="css/header.css">
+  
 </head>
 
 <body>
--- resources/views/backend/partials/left-sidebar.blade.php
+++ resources/views/backend/partials/left-sidebar.blade.php
@@ -1,44 +1,43 @@
 <div class="left-sidebar-pro">
     <nav id="sidebar" class="">
         <div class="sidebar-header">
-            <a href="{{ asset('admin/dashboard') }}"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+            <a href="{{ asset('admin/dashboard') }}"><img class="main-logo size-logo" src="img/logo/logo.png" alt="" /></a>
             <strong><a href="{{ asset('admin/dashboard') }}"></a></strong>
         </div>
         <div class="left-custom-menu-adp-wrap comment-scrollbar">
             <nav class="sidebar-nav left-sidebar-menu-pro">
                 <ul class="metismenu" id="menu1">
                     <li>
-                        <a href="{{ asset('admin/dashboard') }}" aria-expanded="false"><span
-                                class="educate-icon educate-professor icon-wrap"></span> <span
-                                class="mini-click-non">Dashboard</span></a>
+                        <a href="{{ asset('admin/dashboard') }}" aria-expanded="false">
+                            <span class="fa fa-home icon-wrap"></span> 
+                            <span class="mini-click-non">Dashboard</span>
+                        </a>                        
                     </li>
                     <li>
-                        <a class="has-arrow" href="{{ asset('admin/contact') }}" aria-expanded="false"><span
-                                class="educate-icon educate-library icon-wrap"></span> <span
-                                class="mini-click-non">Contact</span></a>
+                        <a class="has-arrow" href="{{ asset('admin/contact') }}" aria-expanded="false">
+                            <span class="fa fa-envelope icon-wrap"></span>
+                            <span class="mini-click-non">Contact</span>
+                        </a>                        
                         <ul class="submenu-angle" aria-expanded="false">
                             <li><a title="Edit Library" href="{{ asset('admin/contact') }}"><span
                                         class="mini-sub-pro">Contact</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="{{ asset('admin/bill') }}" aria-expanded="false"><span
-                                class="educate-icon educate-department icon-wrap"></span> <span
-                                class="mini-click-non">Bill</span></a>
+                        <a class="has-arrow" href="{{ asset('admin/bill') }}" aria-expanded="false">
+                            <span class="fa fa-file icon-wrap"></span>
+                            <span class="mini-click-non">Bill</span>
+                        </a>                                                                      
                         <ul class="submenu-angle" aria-expanded="false">
                             <li><a title="Departments List" href="{{ asset('admin/bill') }}"><span
                                         class="mini-sub-pro">Bill</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a href="{{ asset('admin/oders') }}" aria-expanded="false"><span
-                                class="educate-icon educate-department icon-wrap"></span> <span
-                                class="mini-click-non">Orders</span></a>
-                    </li>
-                    <li>
-                        <a class="has-arrow" href="{{ asset('admin/statistic') }}" aria-expanded="false"><span
-                                class="educate-icon educate-charts icon-wrap"></span> <span
-                                class="mini-click-non">Statistics</span></a>
+                        <a class="has-arrow" href="{{ asset('admin/statistic') }}" aria-expanded="false">
+                            <span class="fa fa-chart-bar icon-wrap"></span>
+                            <span class="mini-click-non">Statistics</span>
+                        </a>                        
                         <ul class="submenu-angle chart-mini-nb-dp" aria-expanded="false">
                             <li><a title="Rounded Charts" href="{{ asset('admin/statistic') }}"><span
                                         class="mini-sub-pro">Statistics</span></a></li>
