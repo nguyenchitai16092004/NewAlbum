0067a225b91b0c09b7a0521cd04a9d91a140e930
LuuNguyen <luunguyen6752@gmail.com>
2025-01-17 21:33:29 +0700

    Update contact
    update contact view profile user

--- resources/views/frontend/pages/account.blade.php
+++ resources/views/frontend/pages/account.blade.php
@@ -21,41 +21,6 @@
     <div class="account-container">
         <div class="account-content">
             <div class="account-card">
-                <h2>Account Information</h2>
-                <div class="account-info">
-                    <div class="account-field name-field">
-                        <label>Full name</label>
-                        <span>{{ session('User')->TenKH }}</span>
-                    </div>
-                    <div class="account-field name-field">
-                        <label>Email</label>
-                        <span style="margin-left: 96px">{{ session('User')->Email }}</span>
-                    </div>
-                    <div class="account-field phone-field">
-                        <label>Phone number</label>
-                        <span style="margin-left: 37px;">{{ session('User')->SDT ?? 'Not provided' }}</span>
-                    </div>
-                    <div class="account-field sex-field">
-                        <label>Sex</label>
-                        <div class="sex-options">
-                            <input type="radio" id="male" name="sex" {{ session('User')->GioiTinh == 1 ? 'checked' : '' }}  style="margin-left: 11px;">
-                            <label for="male">Male</label>
-                    
-                            <input type="radio" id="female" name="sex" {{ session('User')->GioiTinh == 0 ? 'checked' : '' }}>
-                            <label for="female">Female</label>
-                        </div>
-                    </div>                    
-                    <div class="account-field birthday-field">
-                        <label>Birthday</label>
-                        <span style="margin-left: 78px;">{{ session('User')->NgaySinh ?? 'Not provided' }}</span>
-                    </div>
-                    <div class="account-field address-field">
-                        <label>Address</label>
-                        <span style="margin-left: 78px;">{{ session('User')->DiaChiKH ?? 'Not provided' }}</span>
-                    </div>
-                </div>
-            </div>
-            <div class="account-card">
                 <h2>Login Information</h2>
                 <div class="account-info">
                     <div class="account-field email-field">
@@ -69,21 +34,58 @@
                     </div> 
                 </div>
             </div>
+
+            <div class="account-card">
+                <h2>Account Information</h2>
+                <div class="account-info">
+                    <div class="account-field address-field">
+                        <label>Address</label>
+                        <span style="margin-left: 78px;">{{ session('User')->DiaChiKH ?? 'Not provided' }}</span>
+                    </div>
+                    <div class="account-field birthday-field">
+                        <label>Birthday</label>
+                        <span style="margin-left: 78px;">{{ session('User')->NgaySinh ?? 'Not provided' }}</span>
+                    </div>
+                    <div class="account-field sex-field">
+                        <label>Sex</label>
+                        <div class="sex-options">
+                            <input type="radio" id="male" name="sex" {{ session('User')->GioiTinh == 1 ? 'checked' : '' }} style="margin-left: 11px;">
+                            <label for="male">Male</label>
+                    
+                            <input type="radio" id="female" name="sex" {{ session('User')->GioiTinh == 0 ? 'checked' : '' }}>
+                            <label for="female">Female</label>
+                        </div>
+                    </div>                    
+                    <div class="account-field phone-field">
+                        <label>Phone number</label>
+                        <span style="margin-left: 37px;">{{ session('User')->SDT ?? 'Not provided' }}</span>
+                    </div>
+                    <div class="account-field name-field">
+                        <label>Full name</label>
+                        <span>{{ session('User')->TenKH }}</span>
+                    </div>
+                    <div class="account-field name-field">
+                        <label>Email</label>
+                        <span style="margin-left: 96px">{{ session('User')->Email }}</span>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
     <script>
         document.getElementById('togglePassword').addEventListener('click', function() {
-        var passwordInput = document.getElementById('passwordInput');
-        var button = document.getElementById('togglePassword');
-    
-        if (passwordInput.type === 'password') {
-            passwordInput.type = 'text'; // Hiển thị mật khẩu
-            button.innerHTML = 'Hide'; // Đổi chữ thành "Hide"
-        } else {
-            passwordInput.type = 'password'; // Ẩn mật khẩu
-            button.innerHTML = 'Show'; // Đổi chữ thành "Show"
-        }
-    });
+            var passwordInput = document.getElementById('passwordInput');
+            var button = document.getElementById('togglePassword');
+        
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text'; // Hiển thị mật khẩu
+                button.innerHTML = 'Hide'; // Đổi chữ thành "Hide"
+            } else {
+                passwordInput.type = 'password'; // Ẩn mật khẩu
+                button.innerHTML = 'Show'; // Đổi chữ thành "Show"
+            }
+        });
     </script>
 @stop
